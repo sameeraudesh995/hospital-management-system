f947193fbd7649ae934f5bcdbea1de7fa6c666eb
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2024-11-29 22:16:17 +0530

    admin register form created

---
+++ src/com/itp/hms/controller/admin/AdminRegisterFormController.java
@@ -0,0 +1,9 @@
+package com.itp.hms.controller.admin;
+
+import javafx.event.ActionEvent;
+
+public class AdminRegisterFormController {
+    
+    public void handleRegisterButtonAction(ActionEvent actionEvent) {
+    }
+}
