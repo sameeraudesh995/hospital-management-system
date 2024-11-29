29a6982a53d28a9b5864af66bd73b02d53816ac9
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2024-11-29 22:20:03 +0530

    cashier login Form creatde

---
+++ src/com/itp/hms/controller/cashier/CashierLoginForm.java
@@ -0,0 +1,14 @@
+package com.itp.hms.controller.cashier;
+
+import javafx.event.ActionEvent;
+
+public class CashierLoginForm {
+    public void handleSubmitButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void handleClearButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void handleSignUpAction(ActionEvent actionEvent) {
+    }
+}
