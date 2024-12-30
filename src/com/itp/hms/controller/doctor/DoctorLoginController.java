ce124d84d490b3e0a748be0e769533880b9c7dc3
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2024-12-30 06:50:00 +0530

    doctor login page

--- src/com/itp/hms/controller/doctor/DoctorLoginController.java
+++ src/com/itp/hms/controller/doctor/DoctorLoginController.java
@@ -1,4 +1,48 @@
 package com.itp.hms.controller.doctor;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
 public class DoctorLoginController {
+
+    @FXML
+    private BorderPane context;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private Hyperlink forgotPasswordLink;
+
+    @FXML
+    private Hyperlink createAccountLink;
+
+    @FXML
+    private Hyperlink backToHomeLink;
+
+    public void LoginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void ClearOnAction(ActionEvent actionEvent) {
+    }
+
+    public void FrogotPasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    public void BackToHomeOnAction(ActionEvent actionEvent) {
+    }
 }
